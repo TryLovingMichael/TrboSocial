2bc4e027c8f5cb5dac3b0a11823c07ebaa1990ef
TryLovingMichael <trylovingmichael@gmail.com>
2023-04-25 18:13:35 -0600

    Initial Commit

---
+++ TrboSocial/TrboSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrboSocialApp.swift
+//  TrboSocial
+//
+//  Created by Michael Hayden on 2023-04-25.
+//
+
+import SwiftUI
+
+@main
+struct TrboSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
